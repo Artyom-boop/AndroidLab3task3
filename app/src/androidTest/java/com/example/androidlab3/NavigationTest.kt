cfcf9e4a730a5a9e4d2bf9a8b9a8f372a08f77a2
Artyom_Sidorov_3530901/90004 <artyom.sidoroff2014@yandex.ru>
2021-11-23 20:24:22 +0300

    lab4 refactor

---
+++ app/src/androidTest/java/com/example/androidlab3/NavigationTest.kt
@@ -0,0 +1,185 @@
+package com.example.androidlab3
+
+import android.content.pm.ActivityInfo
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testAbout() {
+        launchActivity<MainActivity>()
+        openAbout()
+        onView(withId(R.id.activity_about))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testFragment1() {
+        activityRule.scenario.recreate()
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testFragment2() {
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testFragment3() {
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testBackStack() {
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.bnToFirst)).perform(click())
+        openAbout()
+        pressBack()
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        openAbout()
+        pressBack()
+        pressBack()
+        pressBack()
+        pressBackUnconditionally()
+        assertTrue(activityRule.scenario.state.isAtLeast(Lifecycle.State.DESTROYED))
+    }
+
+    @Test
+    fun testButtonAndView() {
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(doesNotExist())
+        onView(withId(R.id.bnToSecond)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+        onView(withId(R.id.bnToSecond)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).check(doesNotExist())
+        onView(withId(R.id.bnToFirst)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+        onView(withId(R.id.bnToSecond)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(matches(isDisplayed()))
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(doesNotExist())
+        onView(withId(R.id.bnToSecond)).check(doesNotExist())
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+        onView(withId(R.id.tvAbout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun exitFragment1(){
+        pressBackUnconditionally()
+        assertTrue(activityRule.scenario.state.isAtLeast(Lifecycle.State.DESTROYED))
+    }
+
+    @Test
+    fun exitFragment2(){
+        onView(withId(R.id.bnToSecond)).perform(click())
+        pressBack()
+        pressBackUnconditionally()
+        assertTrue(activityRule.scenario.state.isAtLeast(Lifecycle.State.DESTROYED))
+    }
+
+    @Test
+    fun exitFragment3(){
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        pressBack()
+        pressBack()
+        pressBackUnconditionally()
+        assertTrue(activityRule.scenario.state.isAtLeast(Lifecycle.State.DESTROYED))
+    }
+
+    @Test
+    fun recreateFragment1() {
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE }
+        Thread.sleep(1000)
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT }
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun recreateFragment2() {
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE }
+        Thread.sleep(1000)
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun recreateFragment3() {
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE }
+        Thread.sleep(1000)
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/main/java/com/example/androidlab3/Activity2.kt
@@ -0,0 +1,49 @@
+package com.example.androidlab3
+
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+import android.view.Menu
+import com.example.androidlab3.databinding.Activity2Binding
+
+
+class Activity2 : AppCompatActivity() {
+
+    private lateinit var binding: Activity2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = Activity2Binding.inflate(layoutInflater)
+        binding.bnToFirst.setOnClickListener {
+            finish()
+        }
+        binding.bnToThird.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    Activity3::class.java
+                ).setFlags(FLAG_ACTIVITY_CLEAR_TOP)
+            )
+        }
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.activity_about -> {
+                startActivity(Intent(this, ActivityAbout::class.java))
+                true
+            }
+            else -> false
+        }
+    }
+}
